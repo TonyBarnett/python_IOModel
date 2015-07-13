6415bead5a4fceab508de57a86df8feb58466f99
TonyBarnett <tony.barnett@ukplc.net>
2015-07-13 10:46:40 +0100

    matrix algebra

--- models/matrix_functions.py
+++ models/matrix_functions.py
@@ -1,4 +1,5 @@
 import numpy
+from numpy.linalg import inv
 
 
 def get_row_sum(a: numpy.matrix) -> numpy.matrix:
@@ -8,3 +9,14 @@ def get_row_sum(a: numpy.matrix) -> numpy.matrix:
 def get_col_sum(a: numpy.matrix) -> numpy.matrix:
     return numpy.sum(a, axis=0)
 
+
+def element_wise_divide(a: numpy.matrix, b: numpy.matrix) -> numpy.matrix:
+    return numpy.divide(a, b)
+
+
+def matrix_multiply(a: numpy.matrix, b: numpy.matrix) -> numpy.matrix:
+    return a.dot(b)
+
+
+def matrix_divide(a: numpy.matrix, b: numpy.matrix) -> numpy.matrix:
+    return matrix_multiply(a, inv(b))
