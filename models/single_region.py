a991bec533e13e4f9a6c602734d87b52dbacdf5e
TonyBarnett <tony.barnett@ukplc.net>
2015-07-13 10:46:48 +0100

    single region model

--- models/single_region.py
+++ models/single_region.py
@@ -1,23 +1,31 @@
 import numpy
+from numpy.linalg import inv
+from .matrix_functions import get_col_sum, get_row_sum, matrix_divide, matrix_multiply, element_wise_divide
 #  TODO get UK Consumption, UK production, UK emissions and return some carbon intensities
 #  q is column sum of V
 #  g is row sum of V
 #  y is row sum of U
 
 
-def get_row_sum(a: numpy.matrix) -> numpy.matrix:
-    raise NotImplementedError()
-
-
-def get_col_sum(a: numpy.matrix) -> numpy.matrix:
-    return get_row_sum(a.transpose())
-
-
-def run_model(consumption, production, emissions) -> numpy.matrix:
+def run_model(consumption: numpy.matrix, production: numpy.matrix, emissions: numpy.matrix) -> numpy.matrix:
     """
     :param consumption: U
     :param production: V
     :param emissions: e
     :return:
     """
-    numpy.matrix.diagonal()
+    i = numpy.identity(len(consumption))
+
+    q = get_col_sum(production)
+    g = get_row_sum(production)
+
+    y = get_row_sum(consumption)
+
+    b = matrix_divide(consumption, g.diagonal())
+    d = matrix_divide(production, q.diagonal())
+
+    a = matrix_multiply(b, d)
+
+    money_part = matrix_multiply(inv(i - a), y)
+
+    return element_wise_divide(emissions, money_part)
